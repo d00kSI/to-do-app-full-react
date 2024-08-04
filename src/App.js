8bc8d801bcb7d327f07927b1d7b9fe6af565c34f
d00kSI <damijan.kante@gmail.com>
2024-08-04 02:04:57 +0200

    Formed the basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import TodoApp from './TodoApp';
+import JokeApp from './JokeApp';
+import WeatherApp from './WeatherApp';
+
 import './styles.css';
 
 const App = () => {
